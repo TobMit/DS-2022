8e2de4560d79b6010b8477fbac5aeead52cdd039
TobMit <80719513+TobMit@users.noreply.github.com>
2022-03-03 11:05:29 +0100

    Vytvorenie súboru
    Spravene všetko až po úlohu 1

---
+++ Cvicenie 2.sql
@@ -0,0 +1,67 @@
+
+-- osoby ktoré sú študentami
+select meno, priezvisko, ukoncenie, rocnik, case substr(st_skupina,2,1)
+    when 'Z' then 'Zilina'
+    when 'P' then 'Prievidza'
+    when 'R' then 'Ruzomberok'
+    end
+ from os_udaje join student using(rod_cislo)
+    where student.ukoncenie is null
+    order by meno, priezvisko, substr(rod_cislo,1,2);
+
+
+-- vypis mena ucitelov ktoré nem žiadny študent    
+select priezvisko
+  from ucitel 
+    where not exists 
+        (select 'x' from os_udaje join student using (rod_cislo)
+            where ucitel.priezvisko = os_udaje.priezvisko);
+            
+            
+select priezvisko
+    from ucitel
+        where priezvisko not in (
+            select priezvisko from os_udaje join student using(rod_cislo));
+  
+  
+-- predmety ktoré neboli zapísane          
+select cis_predm, nazov
+    from predmet
+        where cis_predm
+            not in (select cis_predm From zap_predmety);
+            
+-- mená ludí ktorý sa narodil v decembri -> 2 podmienky
+
+select meno, priezvisko
+    from os_udaje
+        where substr(rod_cislo, 3,2) = 12 or substr(rod_cislo, 3,2) = 62;
+    
+
+-- mená ludí ktorý sa narodil v decembri -> in    
+select meno, priezvisko
+    from os_udaje
+        where substr(rod_cislo, 3,2) in (12,62);
+        
+-- vypis ucitelov kt.nikdy nic negarantovali
+
+select *
+    from ucitel
+        where os_cislo not in (select garant from predmet_bod);
+        
+        
+--         distinct potlaci duplicity
+select distinct meno, priezvisko, nazov
+    from ucitel join zap_predmety on (prednasajuci = ucitel.os_cislo) join predmet using(cis_predm)
+        order by priezvisko;
+        
+-- Vypisat pocet druhákov
+select count(*)
+    from student
+        where rocnik = '2';
+        
+
+    
+
+
+
+
