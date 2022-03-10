2df37a1bd20db39317b28ca66d9227e8301573ec
TobMit <80719513+TobMit@users.noreply.github.com>
2022-03-10 10:37:42 +0100

    pridanie dalsich uloha
    Pozor vsetky nie funguju

--- Cvicenie 3.sql
+++ Cvicenie 3.sql
@@ -4,3 +4,39 @@ select student.os_cislo
         on (student.os_cislo = zap_predmety.os_cislo) 
             join predmet using (cis_predm)
                 where nazov like '%a%a%a' or nazov like 'A%a%a%'; -- preto lebo prve pismeno bolo velke
+                
+                
+-- vypise os cislo studenta ktory ma predmet s 3 a
+-- potlacenie duplicit cez distinct
+select distinct student.os_cislo
+    from student join zap_predmety 
+        on (student.os_cislo = zap_predmety.os_cislo) 
+            join predmet using (cis_predm)
+                where nazov like '%a%a%a' or nazov like 'A%a%a%'; -- preto lebo prve pismeno bolo velke
+                
+-- vypise os cislo studenta ktory ma predmet s 3 a
+-- potlacenie duplicit inak ako cez distinct
+select student.os_cislo
+    from student st
+        where st.os_cislo in (
+            select * from zap_predmety join predmet on (zap_predmety.cis_predm = predmet.cis_predm)
+              where predmet.nazov like '%a%a%a' or predmet.nazov like 'A%a%a%';  
+              
+
+-- vypise os cislo studenta ktory ma predmet s 3 a
+-- potlacenie duplicit inak ako cez exist
+select os_cislo
+    from student 
+        where exists 
+        (select 'x'
+            from zap_predmety
+             join predmet using (cis_predm)
+              where predmet.nazov like '%a%a%a' or predmet.nazov like 'A%a%a%'
+                and student.os_cislo = zap_predmety.os_cislo);
+                
+ 
+ 
+                
+select meno, priezvisko, rod_cislo
+    from kvete3.p_osoba join kvet3.p_mesto on (kvet3
+              
