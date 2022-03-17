ac5816870bc2152cb7cda9cd625f5a67d50cb348
TobMit <80719513+TobMit@users.noreply.github.com>
2022-03-17 10:27:03 +0100

    2. uloha z cvicenia

--- Cvicenie 4.sql
+++ Cvicenie 4.sql
@@ -9,3 +9,10 @@ update zap_predmety
     set vysledok = 'F', datum_sk = sysdate
         where vysledok is null and cis_predm = 'BA10';
 ROLLBACK;
+
+-- odstranenie vsetky studentov ktori nemaju ziaden predmet
+delete student
+    where student.os_cislo not in (
+        select os_cislo
+            from zap_predmety);
+            
