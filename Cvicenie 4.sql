1911a955bf4f1ae30f94751c860118f5dfd19991
TobMit <80719513+TobMit@users.noreply.github.com>
2022-03-17 10:22:21 +0100

    1. precvicovaci uloha

--- Cvicenie 4.sql
+++ Cvicenie 4.sql
@@ -1,1 +1,11 @@
 -- Cvicenie 3
+-- Zapisat fx vsetkym studentom kotri nemaju znamku z predmetu 
+select *
+    from predmet;
+    
+desc zap_predmety;
+    
+update zap_predmety
+    set vysledok = 'F', datum_sk = sysdate
+        where vysledok is null and cis_predm = 'BA10';
+ROLLBACK;
