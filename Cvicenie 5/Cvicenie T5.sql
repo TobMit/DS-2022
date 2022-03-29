d508eb5de11e218032e470e927e3ce1407d184f2
TobMit <80719513+TobMit@users.noreply.github.com>
2022-03-29 23:11:53 +0200

    Oprava zabudol som naimportovať P_ZTP

--- Cvicenie 5/Cvicenie T5.sql
+++ Cvicenie 5/Cvicenie T5.sql
@@ -3,4 +3,9 @@ select meno,priezvisko
     from p_osoba
         where substr(rod_cislo,3,1) BETWEEN 5 and 6;
         
+-- 5.4.2
+select meno, priezvisko
+    from p_osoba
+        where substr(rod_cislo, 3,2) = extract(month from sysdate) or substr(rod_cislo, 3,2) = (extract(month from sysdate) + 50);
+        
         
