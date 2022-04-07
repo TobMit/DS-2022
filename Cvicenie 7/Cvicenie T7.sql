f01b2e41973d8fc05fb6ed17119652f0adc34f44
TobMit <80719513+TobMit@users.noreply.github.com>
2022-04-07 10:56:59 +0200

    Dalsie ulohy z cvika

--- Cvicenie 7/Cvicenie T7.sql
+++ Cvicenie 7/Cvicenie T7.sql
@@ -35,3 +35,25 @@ select sum(suma), to_char(obdobie,'YYYY'), rod_cislo
     from P_PRISPEVKY join P_POBERATEL using (id_poberatela) join P_OSOBA using (rod_cislo)
         group by to_char(obdobie,'YYYY'), rod_cislo;
 
+-- ci dana osoba dostala viac od socialnej poistovne alebo mala vecie odvoddy
+select sum(suma), to_char(obdobie,'YYYY') as obdobie, rod_cislo
+    from P_ODVOD_PLATBA join P_POISTENIE using (id_poistenca) join P_OSOBA using (rod_cislo)
+        group by to_char(obdobie,'YYYY'), rod_cislo
+    union
+        select sum(suma), to_char(obdobie,'YYYY') as obodobie, rod_cislo
+            from P_PRISPEVKY join P_POBERATEL using (id_poberatela) join P_OSOBA using (rod_cislo)
+                group by to_char(obdobie,'YYYY'), rod_cislo;
+
+-- celkova suma
+-- ku kazdej osobe kolko dostala odvodov podla roku
+select rod_cislo, rok, sum (celkova_suma)
+    from (
+         select -sum(suma) as celkova_suma, to_char(obdobie,'YYYY') as rok, rod_cislo
+            from P_ODVOD_PLATBA join P_POISTENIE using (id_poistenca) join P_OSOBA using (rod_cislo)
+                group by to_char(obdobie,'YYYY'), rod_cislo
+         union
+            select sum(suma), to_char(obdobie,'YYYY') as rok, rod_cislo
+                 from P_PRISPEVKY join P_POBERATEL using (id_poberatela) join P_OSOBA using (rod_cislo)
+                    group by to_char(obdobie,'YYYY'), rod_cislo
+         )
+            GROUP BY rod_cislo, rok;
