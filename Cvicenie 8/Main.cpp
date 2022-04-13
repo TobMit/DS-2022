daf045c521b628510871ad4503646b7868c89079
TobMit <80719513+tobmit@users.noreply.github.com>
2022-04-13 21:13:45 +0200

    Program nafomrátuje mená na prípravu na vlozenie do tabulky

--- Cvicenie 8/Main.cpp
+++ Cvicenie 8/Main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+
 
 using namespace std;
 
 
 int main() {
-    srand(time(NULL));
     ofstream zapisovac;
-    zapisovac.open("day_calendar.unl");
+    fstream citac;
+
+    citac.open("../Cvicenie 8/name_calendar.csv");
+    zapisovac.open("../Cvicenie 8/day_calendar.unl");
+    string  nacitane;
+    string delimiter = ";";
+    if (citac.is_open()) {
+        size_t pos = 0;
+        while (getline(citac,nacitane)) {
+            citac >> nacitane;
+            pos = nacitane.find(delimiter);
+            zapisovac << nacitane.substr(0, pos)<< "|";
+            nacitane.erase(0, pos + delimiter.length());
+            zapisovac << nacitane << "|" << endl;
+        }
+    }
     zapisovac.close();
+    citac.close();
     return 0;
 }
 
