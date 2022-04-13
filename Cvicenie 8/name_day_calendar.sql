2c6fa08dab13331f2f32d66ffc8647e96be1a821
TobMit <80719513+tobmit@users.noreply.github.com>
2022-04-13 22:30:59 +0200

    Naprogramovanie a otestovanie procedúri

--- Cvicenie 8/name_day_calendar.sql
+++ Cvicenie 8/name_day_calendar.sql
@@ -8,5 +8,26 @@ CREATE TABLE name_day_calendar
 );
 commit ;
 
+---------- procedúra vrati meno menin podla datumu --------------------------
+CREATE OR REPLACE FUNCTION name_day(p_datum date)
+RETURN VARCHAR2
+IS NAVR_MENO VARCHAR2(20);
+BEGIN
+    select meno into NAVR_MENO
+        from name_day_calendar
+            where to_char(datum, 'DD/MM') = to_char(p_datum, 'DD/MM');
+    RETURN NAVR_MENO;
+END;
+
+------------------ Volanie proceduri -----------------------------------
+select NAME_DAY(sysdate), to_char(sysdate, 'DD.MM')
+    from DUAL;
+
+--------------- test ci naozaj funguje -------------------------------
+select to_char(datum, 'DD.MM'), meno
+    from name_day_calendar
+        where to_char(datum, 'DD/MM') = to_char(sysdate, 'DD/MM');
 
 
+select to_char(datum, 'DD.MM.YYYY') as datum,meno
+from name_day_calendar;
