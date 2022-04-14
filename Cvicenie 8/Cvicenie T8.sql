1a024391abc9fbb4aa4bcfa04e4922a07fe7753d
TobMit <80719513+tobmit@users.noreply.github.com>
2022-04-14 10:27:56 +0200

    Naporogramovanie funkcie GetVek

--- Cvicenie 8/Cvicenie T8.sql
+++ Cvicenie 8/Cvicenie T8.sql
@@ -13,3 +13,17 @@ end;
 /
 
 set SERVEROUTPUT on;
+
+create or replace function GetVek(p_rc char)
+    return number
+is
+    vstupny_ret char(10);
+    datum_narodenia date;
+BEGIN
+    vstupny_ret:=substr(p_rc, 5, 2) || '.'
+                || mod(substr(p_rc, 3, 2),50)
+                || '.19' || substr(p_rc, 1, 2);
+    datum_narodenia:= to_date(vstupny_ret,'DD.MM.YYYY');
+    return months_between(sysdate, datum_narodenia)/12;
+end;
+/
