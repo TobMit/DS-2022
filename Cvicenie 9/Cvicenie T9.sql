5cd15443f088e2fceb99bb86cf2e135b8de761f5
TobMit <80719513+tobmit@users.noreply.github.com>
2022-04-21 10:32:59 +0200

    Daslie priklady

--- Cvicenie 9/Cvicenie T9.sql
+++ Cvicenie 9/Cvicenie T9.sql
@@ -15,10 +15,30 @@ create or replace trigger suma_
     /
 
 update P_PRISPEVKY set suma = -100;
--- nemusím dávať rollback keďže sa žiadne data nemodifikovali
+-- nemusim davat rollback kedze sa ziadne data nemodifikovali
 alter trigger suma_ disable;
 
 -- chcek constrain ako triger
 alter table P_PRISPEVKY
     add check ( SUMA >= 0 );
 
+-- napiste (pokuste sa) check constraint - atribut kedy nemozeme mat hodnotu v buducnosti (tab. p_prispevky)
+alter table P_PRISPEVKY
+    add check (KEDY <= sysdate);
+-- funkcia sysdate nie je deterministicka preto to nebude fungovat cez chcek constrain
+
+create or replace trigger datum
+    before insert or update on P_PRISPEVKY
+    for each row
+        begin
+            if :new.kedy > sysdate then raise_application_error(-20001, 'chybne zadany datum'); end if;
+        end;
+    /
+
+-- povolit triger ktori sme na zaciatku zakazali
+alter trigger SUMA_ enable;
+-- poradie spustania trigrov nie je garantovane
+-- riesenie: dat dva trigre do jedneho
+-- alebo v novom oracle ma follows a potom to vieme definovat
+
+--
