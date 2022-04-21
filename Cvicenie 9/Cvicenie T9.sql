de47df5ad21069dfe683a1dc3025938e75217247
TobMit <80719513+tobmit@users.noreply.github.com>
2022-04-21 10:23:17 +0200

    Pridanie trigra

--- Cvicenie 9/Cvicenie T9.sql
+++ Cvicenie 9/Cvicenie T9.sql
@@ -2,3 +2,18 @@ SELECT
     * FROM  student;
     
 -- for each row sa viaze iba na riadky kotre sa menia
+
+-- triger, ktory zabezpeci, ze suma prispevov nemoze byt zaporna
+-- ci definujeme trger before alebo after !!!!! BUDE NA TESTE !!!!
+
+create or replace trigger suma_
+    before insert or update on P_PRISPEVKY
+    for each row
+        begin
+            if :new.suma < 0 then raise_application_error(-20001, 'zaporna suma'); end if;
+        end;
+    /
+
+update P_PRISPEVKY set suma = -100;
+-- nemusím dávať rollback keďže sa žiadne data nemodifikovali
+alter trigger suma_ disable;
