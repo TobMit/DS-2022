c55164ce1bc25d14db06a7a01cfae78baee95d5d
TobMit <80719513+tobmit@users.noreply.github.com>
2022-04-28 10:15:12 +0200

    Update z cvika.

--- Cvicenie 10/Cvicenie T10.sql
+++ Cvicenie 10/Cvicenie T10.sql
@@ -4,6 +4,19 @@ create or replace trigger poberatel
     for each row
     begin
         update P_PRISPEVKY set ID_POBERATELA = :new.ID_POBERATELA
-        where ID_POBERATELA := :OLD.ID_POBERATELA;
+        where ID_POBERATELA = :OLD.ID_POBERATELA;
     end;
 /
+
+alter trigger poberatel disable ;
+
+-- DU ID UPDATE PLATITELA!!!!
+
+-- vytvorit pohlad zamestnancov spoločnosti ibm
+create or replace view IBM
+    as select *
+        from P_ZAMESTNANEC
+            where ID_ZAMESTNAVATELA in (select ico
+                                            from P_ZAMESTNAVATEL
+                                                where NAZOV = 'IBM');
+
