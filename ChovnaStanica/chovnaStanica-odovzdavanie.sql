67cd2bc412ccbd074a2e200fa64a458d4fd7db6d
TobMit <80719513+tobmit@users.noreply.github.com>
2022-05-03 11:12:47 +0200

    Priprava na generovanie dat

--- ChovnaStanica/chovnaStanica-odovzdavanie.sql
+++ ChovnaStanica/chovnaStanica-odovzdavanie.sql
@@ -13,3 +13,49 @@ end;
 /
 select ID_POBOCKY, KAPACITA, nvl(obsadenost(ID_POBOCKY),0) as obsadenost
     from POBOCKY ;
+-- triger ktory aktualizuje kapacitu
+
+-- Triger ktorý skontrouje či nieje prevíšena kapacita pobočky
+create or replace trigger kontrola_kapacity
+    before insert or update on ZVIERATA
+    for each row
+    declare
+        cekovaKapacia integer;
+        obsadenostPobocky integer := obsadenost(:new.ID_POBOCKY);
+    begin
+        select KAPACITA into cekovaKapacia
+            from POBOCKY
+                where ID_POBOCKY = :new.ID_POBOCKY;
+        if (cekovaKapacia - obsadenostPobocky) <= 0 then raise_application_error(-20001, 'Pobocka je plna'); end if;
+    end;
+/
+
+
+
+create or replace trigger kontrola_kapacity
+    for insert or update of id_pobocky on ZVIERATA
+compound trigger
+    cekovaKapacia integer;
+    obsadenostPobocky integer;
+    before statement is
+        begin
+            select KAPACITA into cekovaKapacia
+                from POBOCKY
+                    where ID_POBOCKY = :new.ID_POBOCKY;
+            obsadenostPobocky := obsadenost(:new.ID_POBOCKY)
+        end before statement;
+
+    before each row is
+    begin
+        if (cekovaKapacia - obsadenostPobocky) <= 0 then raise_application_error(-20001, 'Pobocka je plna'); end if;
+    end before each row;
+end kontrola_kapacity;
+/
+
+select *
+    from ZVIERATA
+        order by ID_POBOCKY;
+
+update ZVIERATA
+    set ID_POBOCKY = ID_POBOCKY;
+rollback ;
