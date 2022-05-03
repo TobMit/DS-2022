4794aea2b5f7fe657804508ee3efeee8223c1d37
TobMit <80719513+tobmit@users.noreply.github.com>
2022-05-03 12:03:36 +0200

    Naprlenie pomocnýh tabuliek

--- ChovnaStanica/InpExp dat/GenerovanieDat/MainChovnaStanica.cpp
+++ ChovnaStanica/InpExp dat/GenerovanieDat/MainChovnaStanica.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include <fstream>
+#include "dataLoader/dataLoader.h"
+#include <vector>
+#include <array>
 
 using namespace std;
 
+vector<string> menaM, menaZ, ulice, priezviskaM, priezviskaZ;
+vector<array<string,2>> mesta;
+
 void spracujData(string sourceName);
+void naplnPomocneTabulky();
 
-
+void naplnanieTabuliek(vector<string> *vector, DataLoader *loader);
 
 int main() {
+    static const int POCET_ZAZNAMOV = 100000;
+    naplnPomocneTabulky();
+
+
+
+
+
+
     /// Treba vždy zo súboru odstraniť utf 8 bom - cez hex editor prvé tri byty
+    /*
     string sourceName[] = {"fin_operacie",
                             "plemena",
                             "pobocky",
@@ -20,8 +36,50 @@ int main() {
     for (int i = 0; i < 8; i++) {
         spracujData(sourceName[i]);
     }
-
+*/
     return 0;
+}
+
+
+void naplnPomocneTabulky() {
+    DataLoader *loader = new DataLoader("../ChovnaStanica/InpExp dat/GenerovanieDat/sorceData/cities.txt");
+    if (loader->isOpen()) {
+        while (loader->nextLine()){
+            array<string, 2> udaje;
+            udaje.at(0) = loader->getNextParameter();
+            udaje.at(1) = loader->getNextParameter();
+            mesta.push_back(udaje);
+        }
+    }
+    loader->openNew("../ChovnaStanica/InpExp dat/GenerovanieDat/sorceData/mena_muzov.txt");
+    if (loader->isOpen()) {
+        naplnanieTabuliek(&menaM,loader);
+    }
+    loader->openNew("../ChovnaStanica/InpExp dat/GenerovanieDat/sorceData/mena_zien.txt");
+    if (loader->isOpen()) {
+        naplnanieTabuliek(&menaZ,loader);
+    }
+    loader->openNew("../ChovnaStanica/InpExp dat/GenerovanieDat/sorceData/priezviska_muzov.txt");
+    if (loader->isOpen()) {
+        naplnanieTabuliek(&priezviskaM,loader);
+    }
+    loader->openNew("../ChovnaStanica/InpExp dat/GenerovanieDat/sorceData/priezviksa_zien.txt");
+    if (loader->isOpen()) {
+        naplnanieTabuliek(&priezviskaZ,loader);
+    }
+    loader->openNew("../ChovnaStanica/InpExp dat/GenerovanieDat/sorceData/ulice.txt");
+    if (loader->isOpen()) {
+        naplnanieTabuliek(&ulice,loader);
+    }
+    for (int i = 0; i < ulice.size(); i++) {
+        cout << ulice.at(i) << endl;
+    }
+}
+
+void naplnanieTabuliek(vector<string> *vector, DataLoader *loader) {
+    while (loader->nextLine()){
+        vector->push_back(loader->getNextParameter());
+    }
 }
 
 void spracujData(string sourceName) {
