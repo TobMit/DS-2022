c2ac9f1a15dec37e266c7fe3318be2baa2fcd740
TobMit <80719513+tobmit@users.noreply.github.com>
2022-05-12 08:50:57 +0200

    Precvicovanie

--- Cvicenie 11/precvicovanie na test T11.sql
+++ Cvicenie 11/precvicovanie na test T11.sql
@@ -39,3 +39,85 @@ as select *
 select * from ulKarpatska;
 
 -- Vytvorte procedúru, po zadaná rod_cisla, dat_od a dat_do, ktorá vypíše sumu, ktorú osoba zinkasovala za príspevky
+create or replace procedure p_suma_poberatel(rodCislo P_POBERATEL.rod_cislo%type, datOd date, datDo date)
+is
+    scitane number;
+begin
+    select sum(suma) into scitane
+        from P_PRISPEVKY join P_POBERATEL using (id_poberatela)
+            where ROD_CISLO = rodCislo and KEDY >= datOd and kedy <= datDo;
+    DBMS_OUTPUT.PUT_LINE(scitane);-------------------------------------------------------
+end;
+
+
+SET SERVEROUTPUT ON
+
+-- teoreticka otázka
+SELECT meno, priezvisko, COUNT (*)
+    FROM p_osoba JOIN p_poistenie USING (rod_cislo)
+       JOIN p_odvod_platba USING (id_poistenca)
+            group by meno, priezvisko, CIS_PLATBY;
+
+-- zoznam poberateľov, ktorí dostali všetky svoje príspevky viac ako 500 eur
+select distinct ID_POBERATELA, ROD_CISLO
+    from P_POBERATEL join P_PRISPEVKY using (id_poberatela)
+        group by ID_POBERATELA, ROD_CISLO
+            having min(SUMA)> 500;
+
+-- vypíšte zoznam poberateľov, ktorí nedostali príspevok menší ako 5 eur
+select distinct ID_POBERATELA, ROD_CISLO
+    from P_POBERATEL join P_PRISPEVKY using (id_poberatela)
+        group by ID_POBERATELA, ROD_CISLO
+            having min(SUMA) > 5;
+
+-- teoreticka uloho aky prikaz vypise cislo 6 (na vyber boli rozne typy count())
+CREATE TABLE tab (id integer);
+insert into tab values(1);
+insert into tab values(1);
+insert into tab values(2);
+insert into tab values(null);
+insert into tab values(3);
+insert into tab values(null);
+
+select count(*)
+    from tab;
+
+drop table tab;
+
+-- Vytvorte Trigger, ktorý zabezpečí kaskádu príkazov na operáciu Update pri zmene id_okresu
+create or replace trigger t_cascada_id_okresu
+    before insert or update on P_OKRES
+    for each row
+    begin
+        update P_MESTO set ID_OKRESU = :new.id_okresu where ID_OKRESU = :old.id_okresu;
+    end;
+/
+drop trigger t_cascada_id_okresu;
+
+select *
+from P_OKRES;
+
+update P_OKRES
+set ID_OKRESU = 'XX'
+where ID_OKRESU = 'BA';
+
+select *
+from P_MESTO order by ID_OKRESU;
+rollback ;
+
+-- Ku každému typu príspevku vypíšte jeho id, popis, zakladnú výšku príspevku a zoznam všetkých poberateľov,
+-- ktorí poberajú tento typ príspevkov, aj tých ku ktorým neevidujeme žiadneho poberateľa
+select distinct ID_TYPU, ZAKL_VYSKA, POPIS, nvl(ROD_CISLO, '--')
+    from P_TYP_PRISPEVKU left join P_POBERATEL using (id_typu) order by ID_TYPU;
+
+insert into P_TYP_PRISPEVKU (ID_TYPU, ZAKL_VYSKA, POPIS)
+values (5, 100, 'pops');
+
+delete
+from P_TYP_PRISPEVKU
+where ID_TYPU = 5;
+
+select *
+    from P_TYP_PRISPEVKU where ID_TYPU not in (select distinct ID_TYPU from P_POBERATEL);
+select count(*)
+from P_POBERATEL;
